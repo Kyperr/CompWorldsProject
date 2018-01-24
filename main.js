0eac6b00ac2587771e91af6babcbc192133737c5
gpschmitt <27737796+gpschmitt@users.noreply.github.com>
2018-01-23 22:59:27 -0800

    added functions and click listeners

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const var SHOTS_PER_SECOND = 1
+const SHOTS_PER_SECOND = 1
 
 var AM = new AssetManager();
 
@@ -6,7 +6,7 @@ var AM = new AssetManager();
 
 
 
-//Animation moved to its own file.
+//Animation moved to it's own file.
 
 // no inheritance
 function Background(game, spritesheet) {
@@ -97,7 +97,66 @@ function initializePlayerListeners(marine, gameEngine, canvas) {
         // Offset X and Y are based on origin of canvas, as opposed to browser window
         gameEngine.mouseX = e.offsetX;
         gameEngine.mouseY = e.offsetY;
-        
+    });
+
+    canvas.addEventListener("mousedown", function (e) {
+        console.log("Click at (" + e.offsetX + ", " + e.offsetY + ").")
+        var dx = marine.x - e.offsetX;
+        var dy = marine.y - e.offsetY;
+
+        var radiansAngle = Math.atan2(dx, dy);
+        var degreesAngle = radiansToDegrees(radiansAngle);
+
+        marine.isShooting = true;
+        marine.animation.currentAction = "shooting"
+        marine.animation.currentAngle = nearestAngle(degreesAngle, marine.degreesPerAngle);
+    });
+
+    canvas.addEventListener("mouseup", function (e) {
+        marine.isShooting = false;
+        marine.animation.currentState = "standing";
     });
     
+}
+
+/*
+ * Returns the angle (in degrees) closest to theDegrees
+ * that is a multiple of incrementAmount.
+ * For example, nearestAngle(46.2, 22.5) would return 45. 
+ */
+function nearestAngle(theDegrees, incrementAmount) {
+    // Start at 0
+    var nearestDegree = 0;
+
+    // Continuously increment by incrementAmount until degrees is passed or met
+    while (nearestDegree < theDegrees) {
+        nearestDegree += incrementAmount;
+    }
+    
+    // Calculate which is closer, the current nearestDegree or the prior one
+    var currentDistance = Math.abs(theDegrees - nearestDegree);
+    var priorDistance = Math.abs(theDegrees - (nearestDegree - incrementAmount));
+
+    // If prior nearestDegree was closer to degrees, decrement by incrementAmount
+    if (priorDistance < currentDistance) {
+        nearestDegree -= incrementAmount;
+    }
+
+    return nearestDegree;
+}
+
+/*
+ * Converts radians to degrees.
+ * For example, radiansToDegrees(0) returns 0, radiansToDegrees(Math.pi) returns 180.
+ */
+function radiansToDegrees(radians) {
+    return radians * (180 / Math.pi)
+}
+
+/*
+ * Converts degrees to radians.
+ * For example, degreesToRadians(0) returns 0, degreesToRadians(180) returns Math.pi.
+ */
+function degreesToRadians(degrees) {
+    return (180 / Math.pi) / degrees
 }
